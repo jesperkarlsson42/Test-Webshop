07411e9be3d171a234209364b253f1941968653b
Carolina Lagerberg <carolina.lagerberg@live.se>
2021-02-22 22:12:26 +0100

    corrected productList in userSchema

--- route/productRoute.js
+++ route/productRoute.js
@@ -1,13 +1,17 @@
-const {addProductForm, addProductFormSubmit, showProducts, addToShoppingCart} = require("../controller/productController")
+const {
+  addProductForm,
+  addProductFormSubmit,
+  showProducts,
+  addToShoppingCart,
+} = require("../controller/productController");
 
 const express = require("express");
 const router = express.Router();
 
-router.get("/addProduct", addProductForm)
-router.post("/addProduct", addProductFormSubmit)
+router.get("/addProduct", addProductForm);
+router.post("/addProduct", addProductFormSubmit);
 router.get("/showProducts", showProducts);
 router.get("/showShoppingCart", addToShoppingCart);
 router.get("/addToCart/:id", addToShoppingCart);
-
 
 module.exports = router;
