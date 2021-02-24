35d627bc8d253fd295bb83606cff7f5317b4703d
Carolina Lagerberg <carolina.lagerberg@live.se>
2021-02-24 15:50:27 +0100

    added product link

--- controller/productController.js
+++ controller/productController.js
@@ -4,44 +4,55 @@ require("dotenv").config();
 //const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const addProductForm = (req, res) => {
-
-res.render("productForm.ejs", {err:" "})
-}
+  res.render("productForm.ejs", { err: " " });
+};
 
 const addProductFormSubmit = async (req, res) => {
+  const { name, description, price } = req.body;
 
-    const {name, description, price} = req.body
+  const product = await new Product({
+    name: name,
+    description: description,
+    price: price,
+  }).save();
 
-    const product = await new Product({name:name, description:description, price:price}).save();
+  const user = await User.findOne({ _id: req.user.user._id });
 
-    const user = await User.findOne({_id:req.user.user._id})
+  user.addProductList(product._id);
+  //console.log(user)
 
-    user.addProductList(product._id);
-    //console.log(user)
+  res.redirect("/showProducts");
+};
 
-    res.redirect("/showProducts")
-}
+const showProducts = async (req, res) => {
+  const products = await Product.find();
 
-const showProducts = async (req, res) =>{
-    const products = await Product.find()
- 
-     res.render("productPage.ejs", {err:" ", products: products})
- }
+  res.render("productPage.ejs", { err: " ", products: products });
+};
 
- const addToShoppingCart = async (req, res) => {
+const addToShoppingCart = async (req, res) => {
+  const productId = req.params.id;
+  //console.log("hej", req.user.user)
 
-    const productId = req.params.id
-    //vi ska spara course ID in i user collectionen
+  const user = await User.findOne({ _id: req.user.user._id });
+  //console.log("hej", user)
 
-    const user = await User.findOne({_id:req.user.user._id})
-    //console.log(user)
+  user.addToCart(productId);
+  //console.log(user);
 
-    user.addToCart(productId);
-    //console.log(user);
+  const userWithProductData = await User.findOne({
+    _id: req.user.user._id,
+  }).populate("shoppingCart");
+  //console.log(userWithProductData.shoppingCart)
+  res.render("shoppingCart.ejs", {
+    cartItem: userWithProductData.shoppingCart,
+    err: "",
+  });
+};
 
-    const userWithProductData = await User.findOne({_id:req.user.user._id}).populate("shoppingCart");
-    //console.log(userWithCourseData.shoppingCart)
-    res.render("shoppingCart.ejs", {cartItem: userWithProductData.shoppingCart, err: ""})
-}
-
-module.exports = {addProductForm, addProductFormSubmit, showProducts, addToShoppingCart}
+module.exports = {
+  addProductForm,
+  addProductFormSubmit,
+  showProducts,
+  addToShoppingCart
+};
--- route/productRoute.js
+++ route/productRoute.js
@@ -2,16 +2,17 @@ const {
   addProductForm,
   addProductFormSubmit,
   showProducts,
-  addToShoppingCart,
+  addToShoppingCart
 } = require("../controller/productController");
 
 const express = require("express");
 const router = express.Router();
+const verifyUser = require("../middleware/verifyUser");
 
-router.get("/addProduct", addProductForm);
-router.post("/addProduct", addProductFormSubmit);
-router.get("/showProducts", showProducts);
-router.get("/showShoppingCart", addToShoppingCart);
-router.get("/addToCart/:id", addToShoppingCart);
+router.get("/addProduct", addProductForm, verifyUser);
+router.post("/addProduct", addProductFormSubmit, verifyUser);
+router.get("/showProducts", showProducts, verifyUser);
+router.get("/showShoppingCart", addToShoppingCart, verifyUser);
+router.get("/addToCart/:id", addToShoppingCart, verifyUser);
 
 module.exports = router;
